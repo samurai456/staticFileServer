3326f3d92dd3226759a77720bac260dfbbaff696
samurai456 <ula.sadikov2001@gmail.com>
2023-06-28 20:03:53 +0500

    's'

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const upload = multer({dest: 'stat/'})
+const cors = require('cors')
+
+const app = express()
+
+app.use('/static', express.static(path.join(__dirname, '/stat')))
+app.use(cors({
+    origin: '*',
+    methods: ['POST', 'PUT', 'GET', 'DELETE'],
+    credentials: true,
+}));
+
+app.post('/upload', upload.single('file'), (req, res)=>{
+    console.log(req.file)
+    res.send({type: 'filename', name: req.file.filename})
+});
+
+app.listen(8500, ()=>console.log('server is running'))
